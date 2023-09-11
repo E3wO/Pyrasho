d78b72b3bb6d8f5343c53c90f289af82a1a8ff6b
Eero Muikku <euromuikku@hotmail.com>
2023-09-11 14:26:50 +0300

    making scaling to different resolutions

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -4,7 +4,7 @@ import React, { useEffect } from "react";
 import Phaser from "phaser";
 import { mapToLoad } from "./MapLoader";
 import PlayerCharacter from "../components/gameplay/PlayerCharacter";
-import { width, height } from "../config/gameConfig";
+import { width, height, multiplier } from "../config/gameConfig"; // Screen width, height, and multiplier
 
 class GameScene extends Phaser.Scene {
   constructor() {
@@ -21,12 +21,13 @@ class GameScene extends Phaser.Scene {
   }
 
   create() {
-    this.physics.world.setBounds(0, 0, width, height);
-    this.add.image(width / 2, height / 2, "ground");
+    this.physics.world.setBounds(0, 0, width, height); // Set collision to edges of the screen
+    this.ground = this.add.image(width / 2, height / 2, "ground"); // Adds ground texture and makes it centered
+    this.ground.setScale(multiplier)
 
     this.wallGroup = this.physics.add.staticGroup();
 
-    const walls = width / 32;
+    const walls = width / (32 * multiplier);
     for (let i = 0; i < walls; i++) {
       const wallX = 16 + 32 * i;
       this.wallGroup.create(wallX, 16, "wall");
@@ -34,8 +35,9 @@ class GameScene extends Phaser.Scene {
     console.log("Creating in GameScene.js");
 
     this.player = new PlayerCharacter(this, 100, 100);
+    this.player.setScale(multiplier)
 
-    this.physics.add.collider(this.player, this.wallGroup);
+    this.physics.add.collider(this.player, this.wallGroup); // Makes player and walls collide
   }
 
   update() {
