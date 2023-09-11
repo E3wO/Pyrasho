b847978b7a8ba98c37093e60dc904f638d01d0a7
Eero Muikku <euromuikku@hotmail.com>
2023-09-11 13:47:12 +0300

    added description to MapLoader.js

--- src/scenes/MapLoader.js
+++ src/scenes/MapLoader.js
@@ -1,6 +1,8 @@
+// MapLoader.js - Determines which map/level to load
+
 function mapToLoad() {
-  console.log("MapLoader")
-  return("assets/ground.png")
+  console.log("MapLoader");
+  return "assets/ground.png";
 }
 
-export {mapToLoad}
+export { mapToLoad };
