3580ae2facadac04f6f5c09424c54524e5726b71
Eero Muikku <euromuikku@hotmail.com>
2023-09-18 14:28:44 +0300

    refactored logic into methods

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -10,10 +10,6 @@ class GameScene extends Phaser.Scene {
     super("GameScene");
   }
 
-  init(data) {
-    this.onSignal = data.onSignal;
-  }
-
   preload() {
     const mapPath = mapToLoad();
     this.load.image("ground", mapPath);
@@ -23,42 +19,45 @@ class GameScene extends Phaser.Scene {
   }
 
   create() {
-    this.physics.world.setBounds(0, 0, width, height); // Set collision to edges of the screen
-    this.ground = this.add.image(width / 2, height / 2, "ground"); // Adds ground texture and makes it centered
-    this.ground.setScale(multiplier);
+    this.setupBackground();
+    this.setupPlayer();
+    this.setupInteractiveElements();
+  }
 
-    const bWidth = width / (32 * multiplier); // Determine screen width in blocks
-    const bHeight = height / (32 * multiplier) //Determine screen height in blocks
+  setupBackground() {
+    this.physics.world.setBounds(0, 0, width, height);
+    this.addScaledImage(width / 2, height / 2, "ground", multiplier);
+
+    const bWidth = width / (32 * multiplier);
+    const bHeight = height / (32 * multiplier);
 
     for (let i = 0; i < bWidth; i++) {
       const wallX = (16 * multiplier) + (32 * multiplier) * i;
-      const wallY = (16 * multiplier)
-      this.seina = this.add.image(wallX, wallY, "wall");
-      this.seina.setScale(scale)
+      this.addScaledImage(wallX, 16 * multiplier, "wall");
 
       for (let n = 1; n < bHeight; n++) {
-        const floorY = wallY + (32 * multiplier) * n;
-        this.floor = this.add.image(wallX, floorY, "floor");
-        this.floor.setScale(scale)
+        this.addScaledImage(wallX, (16 + 32 * n) * multiplier, "floor");
       }
     }
+  }
 
-    console.log("Creating in GameScene.js");
-
-    const backbutton = this.add
-      .text((width / 16) * 11, 60, "Back to Main Menu")
-      .setInteractive();
-    backbutton.on(
-      "pointerdown",
-      function () {
-        this.scene.start("MainMenu")
-      }.bind(this)
-    );
-
+    setupPlayer() {
     this.player = new PlayerCharacter(this, 100 * multiplier, 100 * multiplier);
     this.player.setScale(scale);
-
     //this.physics.add.collider(this.player, this.object); // Makes player and desired objects collide
+  }
+
+  setupInteractiveElements() {
+    const backbutton = this.add.text((width / 16) * 11, 60, "Back to Main Menu").setInteractive();
+    backbutton.on("pointerdown", function () {
+      this.scene.start("MainMenu");
+    }, this);
+  }
+
+  addScaledImage(x, y, key, scaleValue = scale) {
+    const image = this.add.image(x, y, key);
+    image.setScale(scaleValue);
+    return image;
   }
 
   update() {
